572d3a276f4cc8c89b6aec5a67495f76ecddff64
Kenneth Long <kdlong@hep.wisc.edu>
2025-04-17 09:34:04 -0400

    Script to make multiplicative corrections

--- utilities/common.py
+++ utilities/common.py
@@ -278,7 +278,7 @@ def get_dilepton_ptV_binning(fine=False):
     return (
         [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 20, 23, 27, 32, 40, 54, 100]
         if not fine
-        else range(60)
+        else range(101)
     )
 
 
